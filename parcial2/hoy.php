7b77355b969d0072388b88368c06dc460b20f5f2
ycarlos21 <carlos20461@cbtis75.edu.mx>
2022-09-30 09:15:39 -0500

    30 sep

---
+++ parcial2/hoy.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<p class="fecha">';
+        echo date('d-m-y');
+        echo '<br>';
+        echo 'Carlos Benito Ramirez Vazquez 5 PM';
+        echo '</p>';
+    ?>
+</body>
+</html>
